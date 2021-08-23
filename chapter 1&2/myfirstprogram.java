e84fb110e0cb76343deb99826ec12cca8597e397
prao1913 <50085385+prao1913@users.noreply.github.com>
2021-08-23 15:02:22 -0500

    Added my first program/class

---
+++ chapter 1&2/myfirstprogram.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class myfirstprogram here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class myfirstprogram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is a test this is only a test");
+        System.out.println("This is a test again!");
+    }
+}
