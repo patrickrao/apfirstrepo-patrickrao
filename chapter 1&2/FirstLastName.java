1f6c025a8a02f79c5397a3b3a14a50616a183f50
prao1913 <50085385+prao1913@users.noreply.github.com>
2021-09-07 07:40:39 -0500

    First Middle Last

--- chapter 1&2/FirstLastName.java
+++ chapter 1&2/FirstLastName.java
@@ -1,15 +1,33 @@
 
 /**
- * Write a description of class FirstLastName here.
+ * asks user for first and last name, then displays components
  *
- * @author (your name)
+ * Patrick
  * @version (a version number or a date)
  */
+
+import java.util.*;
+import java.util.Scanner;
+
 public class FirstLastName
 {
     public static void main(String[] args) {
-        //code
-        //code
+        Scanner Input = new Scanner(System.in);
+        String fullname = Input.nextLine();
+
+        // finding position of 1st space, and then displaying info before it
+        int a = fullname.indexOf(' ');
+        String first = fullname.substring(0, a);
+        int b = (fullname.substring(a)).indexOf(' ');
+        // String middle = fullname.sub
+        System.out.print(fullname.substring(b));
+        String middle = fullname.substring(a,b);
+        int c = fullname.substring(b).indexOf(' ');
+        String last = fullname.substring(b+1);
+        System.out.print("First " + first);
+        System.out.print("Middle " + middle);
+        System.out.print("Last " + last);
+        
         
         
      
